aa2e8fa6e8f7dea98f44cd0bdbdcaba44da76953
DACHXY <Danny10132024@gmail.com>
2023-11-09 17:22:55 +0800

    Change relative path to absolute path

--- Installer.ps1
+++ Installer.ps1
@@ -72,3 +72,5 @@ InstallChocolateyAndImportModule
 InstallOhMyPosh
 RestoreWTConfig
 RestoreUserFileStructure
+
+Pause
--- run.ps1
+++ run.ps1
@@ -1,12 +1,18 @@
+$fileRoot = Split-Path -parent $MyInvocation.MyCommand.Definition
+
 # install Package Provider
 Install-PackageProvider -Name NuGet -Force
 Install-Module -SkipPublisherCheck -Name Microsoft.WinGet.Client -Force
 
 # Get Winget Newest and Install
 Write-Host "Downloading AppInstaller package..."
-(New-Object Net.WebClient).Downloadfile("https://aka.ms/getwinget", "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle")
+$desPath = "$fileRoot\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+(New-Object Net.WebClient).Downloadfile("https://aka.ms/getwinget", $desPath)
 Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -InstallAllResources
 Write-Host "[DONE] AppInstaller package Installed."
+Write-Host "Clean up installer package..."
+Remove-Item $desPath
+Write-Host "[Done] Cleaned up."
 
 # Install the powershell
 winget install -e --id Microsoft.PowerShell  --accept-package-agreements --accept-source-agreements
@@ -14,5 +20,7 @@ winget install -e --id Microsoft.PowerShell  --accept-package-agreements --accep
 # Install the windows terminal preview
 winget install -e --id Microsoft.WindowsTerminal.Preview  --accept-package-agreements --accept-source-agreements
 
+
+Write-Host "Installing Apps & Environment"
 # Start installer with pwsh
-Start-Process pwsh -ArgumentList "-File Installer.ps1"
+Start-Process pwsh "$FileRoot\Installer.ps1"
